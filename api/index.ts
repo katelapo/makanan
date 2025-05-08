5a06dfd60347d32f96ab0dc8759af22cc50cceef
makanan <hubillahpunk@gmail.com>
2025-05-08 15:45:31 +0700

    membuat data base

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,17 @@
 interface Env {
     ASSETS: Fetcher;
+    DB: D1Database;
   }
   
   export default {
     fetch(request, env) {
+    async fetch(request, env) {
       const url = new URL(request.url);
   
       if (url.pathname.startsWith("/api/index.ts")) {
-        return Response.json([
-          
-        ]);
+        let { results } = await env.DB.prepare("SELECT * FROM events").all();
+        return Response.json(results);
+       }
       }
   
       return env.ASSETS.fetch(request);
